68af570d7d34058148a2577931b78f99f6acd50e
mJota666 <micode2744@gmail.com>
2025-03-17 19:45:38 +0700

    LTW-15 Make basic layout for variants

---
+++ Navigation/Converters/VariantNameConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using PhoneSelling.Data.Models;
+
+namespace Navigation.Converters
+{
+    public class VariantNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is Variant variant)
+            {
+                // Get the parent item's name, the storage, and the color name.
+                // Assuming 'variant.Item' is set, and 'variant.Storage' is a string.
+                string itemName = variant.Item != null ? variant.Item.ItemName : "";
+                string storage = variant.Storage;
+                string colorName = "";
+
+                if (variant.Color != null)
+                {
+                    // If the Color class has a "Name" property, use it.
+                    var prop = variant.Color.GetType().GetProperty("Name");
+                    if (prop != null)
+                    {
+                        colorName = prop.GetValue(variant.Color)?.ToString() ?? "";
+                    }
+                    else
+                    {
+                        colorName = variant.Color.ToString();
+                    }
+                }
+
+                return $"{itemName} - {storage} - {colorName}";
+            }
+            return "";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
