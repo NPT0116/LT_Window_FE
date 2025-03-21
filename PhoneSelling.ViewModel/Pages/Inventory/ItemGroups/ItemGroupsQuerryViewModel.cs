d16c2344b2afbf43993d0692a2e86b62be84d664
mJota666 <micode2744@gmail.com>
2025-03-22 04:20:16 +0700

    merge LWT22 with main

--- PhoneSelling.ViewModel/Pages/Inventory/ItemGroups/ItemGroupsQuerryViewModel.cs
+++ PhoneSelling.ViewModel/Pages/Inventory/ItemGroups/ItemGroupsQuerryViewModel.cs
@@ -1,17 +1,18 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using PhoneSelling.Data.Common.Internal.Responses;
-//using PhoneSelling.Data.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PhoneSelling.Data.Common.Internal.Responses;
+using PhoneSelling.Data.Models;
+using PhoneSelling.Data.Repositories.ItemGroupRepository.ApiService.Contracts.Requests;
 
-//namespace PhoneSelling.ViewModel.Pages.Inventory.ItemGroups
-//{
-//    public partial class ItemGroupsQuerryViewModel : PaginationQueryViewModel<ItemGroup, ItemGroupPaginationQuery>
-//    {
-//        public ItemGroupsQuerryViewModel(Func<ItemGroupPaginationQuery, Task<PaginationResult<ItemGroup>>> fetchDataFunc) : base(fetchDataFunc)
-//        {
-//        }
-//    }
-//}
+namespace PhoneSelling.ViewModel.Pages.Inventory.ItemGroups
+{
+    public partial class ItemGroupsQuerryViewModel : PaginationQueryViewModel<ItemGroup, ItemGroupsPaginationQuery>
+    {
+        public ItemGroupsQuerryViewModel(Func<ItemGroupsPaginationQuery, Task<PaginationResult<ItemGroup>>> fetchDataFunc) : base(fetchDataFunc)
+        {
+        }
+    }
+}
